746f81ac2c2a30d101a277c549bc03b4f8cea4f1
Bathala-Haresh <hareshbathala87@gmail.com>
2021-08-10 14:11:51 +0530

    Added:program to find cheapest best rated hotel

--- src/main/java/com/hotelreservation/HotelReservationService.java
+++ src/main/java/com/hotelreservation/HotelReservationService.java
@@ -59,7 +59,38 @@ public class HotelReservationService {
         return results.stream().filter(result -> result.getTotalRate() == results.get(0).getTotalRate())
                 .collect(Collectors.toList());
     }
+    /****************************************************************
+     * this list method is used to find the cheapest best hotel depending on ratings
+     * @Param CustomerType two types of customers regular and reward
+     * @Param initialDate is used to get date of the customer initial date
+     * @Param endDate is used to get customer end date
+     * @return displays all the hotels list which are cheap and best rated.
+     ***************************************************************/
+    public List<Result> findCheapestBestRatedHotel(CustomerType customerType,
+                                                   String initialDateRange,
+                                                   String endDateRange) {
+        LocalDate initialDate = LocalDate.parse(initialDateRange, DATE_RANGE_FORMAT);
+        LocalDate endDate = LocalDate.parse(endDateRange, DATE_RANGE_FORMAT);
 
+        List<Result> results = this.hotels.stream()
+                .map(hotel -> {
+                    Result result = new Result();
+                    result.setHotelName(hotel.getName());
+                    result.setTotalRate(hotel.getTotalRate(customerType, initialDate, endDate));
+                    result.setRating(hotel.getRating());
+                    return result;
+                })
+                .sorted(Comparator.comparing(Result::getTotalRate).thenComparing(Result::getRating,
+                        Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+
+        return results.stream()
+                .filter(result ->
+                        result.getTotalRate() == results.get(0).getTotalRate()
+                                && result.getRating() == results.get(0).getRating())
+                .collect(Collectors.toList());
+    }
+    
     private LocalDate getParsedDate(String dateString) {
         try {
             return LocalDate.parse(dateString, DATE_RANGE_FORMAT);
